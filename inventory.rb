852c2c51ac07fa52e85a8617ba34db8bee556d18
Steven Masse <semasse2000@yahoo.com>
2019-08-01 19:44:57 -0500

    changed from arrays to inventory

---
+++ inventory.rb
@@ -0,0 +1,51 @@
+require_relative 'vehicle'
+
+cars9 = Vehicles.new("2008", "ford", "fiesta", "14,260", "brown")
+cars10 = Vehicles.new("2014", "ford", "fusion", "28,840", "dark green")
+car11 = Vehicles.new("2009", "ford", "mustang", "26,995", "light blue")
+cars12 = Vehicles.new("2013", "ford", "taurus", "27,700", "gold")
+
+$allcars = [["2009", "ford", "fiesta", "14,260", "yellow"],
+            ["2018","ford", "fusion", "22,840", "blue"],
+            ["2005", "ford", "mustang", "26,395", "red"],
+            ["2017", "ford", "taurus", "27,800", "white"],
+            ["2006", "ford", "fiesta", "15,260", "green"],
+            ["2016", "ford", "fusion", "22,940", "orange"],
+            ["2007", "ford", "mustang", "26,495", "black"],
+            ["2015", "ford", "taurus", "26,800", "silver"], ]
+
+=begin
+truck1 = Vehicles.new("2019", "ford", "ranger", "40,430", "truck", "black")
+truck2 = Vehicles.new("2016", "ford", "transit connect", "26,845", "truck", "white")
+truck3 = Vehicles.new("2017", "ford", "f150", "28,155", "truck", "silver")
+truck4 = Vehicles.new("2013", "ford", "super duty", "33,150", "truck", "dark grey")
+truck5 = Vehicles.new("2015", "ford", "transit", "35,840", "truck", "dark red")
+truck6 = Vehicles.new("2018", "ford", "ranger", "20,890", "truck", "silver")
+truck7 = Vehicles.new("2017", "ford", "transit connect", "26,845", "truck", "red")
+truck8 = Vehicles.new("2014", "ford", "f150", "28,155", "truck", "black")
+truck9 = Vehicles.new("2020", "ford", "super duty", "33,150", "truck", "dark green")
+truck10 = Vehicles.new("2016", "ford", "transit", "35,840", "truck", "dark blue")
+truck11 = Vehicles.new("2015", "ford", "ranger", "17,650", "truck", "orange")
+truck12 = Vehicles.new("2018", "ford", "transit connect", "26,845", "truck", "gold")
+$alltrucks = [truck1, truck2, truck3, truck4, truck5, truck6, truck7, truck8, truck9, truck10, truck11, truck12]
+
+suv1 = Vehicles.new("2019", "ford", "edge", "29,995", "suv", "gold")
+suv2 = Vehicles.new("2018", "ford", "escape", "24,215", "suv", "black")
+suv3 = Vehicles.new("2017", "ford", "explorer", "32,365", "suv", "white")
+suv4 = Vehicles.new("2016", "ford", "flex", "30,575", "suv", "silver")
+suv5 = Vehicles.new("2015", "ford", "expedition", "52,130", "suv", "blue")
+suv6 = Vehicles.new("2014", "ford", "edge", "29,995", "suv", "red")
+suv7 = Vehicles.new("2013", "ford", "escape", "24,215", "suv", "dark red")
+suv8 = Vehicles.new("2012", "ford", "explorer", "32,365", "suv", "dark green")
+suv9 = Vehicles.new("2020", "ford", "flex", "30,575", "suv", "dark blue")
+suv10 = Vehicles.new("2016", "ford", "expedition", "52,130", "suv", "orange")
+suv11 = Vehicles.new("2018", "ford", "edge", "29,995", "suv", "tan")
+suv12 = Vhicles.new("2014", "ford", "escape", "24,215", "suv", "grey")
+$allsuvs = [suv1, suv2, suv3, suv4, suv5, suv6, suv7, suv8, suv9, suv10, suv11, suv12]
+
+#all_cars.each do |car|
+#    puts car
+#end
+
+#puts cheap_Car
+=end
