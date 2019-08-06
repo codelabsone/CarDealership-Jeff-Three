0e261daf2fd1c31a0da4759eb5f1f95c4496d5b4
Steven Masse <semasse2000@yahoo.com>
2019-08-06 00:44:11 -0500

    Streamlined the user menu.  Added ability to search array in inventory.rb file correctly.  Made a .each do, to loop through variables pulled and print them out to a user friendly string.  Revised first user input to search for cars, trucks or suvs.  Changed second user input to narrow down filters.  Added ability for customer to restart menu to do another search.

--- usermenu.rb
+++ usermenu.rb
@@ -1,35 +1,66 @@
 require 'colorize'
-require_relative 'vehicle'
+require_relative 'inventory'
 
+#Asks the user to input a cars, trucks or suvs to pull array information.
 def menu
   puts "-----------------------------------------------------------------------"
-  puts "What make of vehicle are you looking for today?".colorize(:green)
+  puts "Would you like to search our inventory of cars, trucks or suvs?".colorize(:green)
   prompt = "> "
   print prompt
   answer = gets.chomp.downcase
-  selection = $inventory.select{|s| s == answer}
-if selection.size > 0
+  selection = $inventory.select{|r| r.include? answer}
+
+#If the input from above is greater than 0 it asks gives the inventory of the inventory. And uses .each to print it out to a string is a user friendly format.
+  if selection.size > 0
   puts "-----------------------------------------------------------------------"
-  puts "These are the #{answer.capitalize}s we have available:".colorize(:green)
+  puts "These are the #{answer.capitalize} we have available:".colorize(:green)
   result = $inventory.select{|x| x.include? answer}
-  print result.uniq
-  puts "Which model would you like to view?".colorize(:green)
+      result.each do |car|
+      year = car[0]
+      model = car[2]
+      cost = car[3]
+      color = car[4]
+  puts "We have a #{year} #{model} in the color #{color} for $#{cost}."
+  end
+
+#Ask the customer if they want to further narrow their search and prints it out into a user friendly format.
+  puts "-----------------------------------------------------------------------"
+  puts "You can narrow our inventory by year, model or color.".colorize(:green)
+  puts "Examples: red, 2014, edge, etc.".colorize(:green)
   print prompt
   input = gets.chomp.downcase.gsub(/[-!@$%&*?]/, "")
   puts "-----------------------------------------------------------------------"
-  puts "These are the options we have available for the #{input.capitalize}:".colorize(:green)
+  puts "These are the options we have available for #{input.capitalize}:".colorize(:green)
   new_result = $inventory.select{|x| x.include? input}
-  print new_result.uniq
+      new_result.each do |car|
+      year = car[0]
+      model = car[2]
+      cost = car[3]
+      color = car[4]
+      puts "We have a #{year} #{model} in the color #{color} for $#{cost}."
+  end
+
+#Ask the customer if they want to search for something else and if yes loops back to beginning and if not outputs ending from cardealership.rb.
   puts "-----------------------------------------------------------------------"
+  puts "Would you like to search for another vehicle?"
+  print prompt
+  sure = gets.chomp.downcase
+  if sure =="yes"
+  puts menu
+  elsif sure =="no"
+
+#If the customer input from the narrow search function has a variable that can't match to the array, it prints this output and if yes will bring the customer back to the beginning or output the ending from cardealership.rb.
 else
-  puts "Sorry, we don't carry that make.".colorize(:green)
+  puts "Sorry, we don't carry that.".colorize(:green)
   puts "Would you like to search for another vehicle?".colorize(:green)
   print prompt
   sure = gets.chomp.downcase
   if sure == "yes"
-    menu
+    print prompt
+    puts "-----------------------------------------------------------------------"
+    puts menu
   else
-    puts "Thank you for shopping with us today!".colorize(:green)
+      end
+    end
   end
-end
 end
