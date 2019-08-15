135541f0f384431e64e93301f955edc564280697
Steven Masse <semasse2000@yahoo.com>
2019-08-15 17:33:41 -0500

    Scrapped old coding and redid coding for adding a vehicle along with a user menu interface.

--- add_inventory.rb
+++ add_inventory.rb
@@ -1,14 +1,29 @@
-class AddInventory
-​
-  attr_accessor :vehic
-​
-  def initialize
-    @vehic = []
-  end
-  def add_vehic(vehicle)
-    @vehic.push(vehicle)
-  end
-  def sell_vehic(vehicle)
-    @vehic = @vehic.reject{ |v| v == vehicle}
+require_relative 'inventory'
+
+def verifyvehicle
+  print 'What is your car year? '
+  caryear = gets.chomp
+  print 'What is your car make? '
+  carmake = gets.chomp
+  print 'What is your car model? '
+  carmodel = gets.chomp
+  print 'What is your car cost? '
+  carcost = gets.chomp
+  print 'What is your car color? '
+  carcolor = gets.chomp
+  print 'What is your car type? '
+  cartype = gets.chomp
+  puts "You said a #{caryear}, #{carmake}, #{carmodel} for $#{carcost} in the color #{carcolor} under the section #{cartype}, is that correct? (Yes or No)"
+  verify = gets.chomp
+if verify == "yes"
+  inventory = $inventory
+  puts "Thank you, your vehicle has been added."
+  $inventory << ["#{caryear}", "#{carmake}", "#{carmodel}", "#{carcost}", "#{carcolor}", "#{cartype}"]
+  File.write('inventory.rb', "$inventory = #{inventory}")
+else
+  puts "Please reenter your vehicle."
+  verifyvehicle
   end
 end
+
+verifyvehicle
